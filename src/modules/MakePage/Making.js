258782509f7c40c57a4270acb9ca9e912ede9b76
matteo <matteo.yuen@gmail.com>
2024-11-19 15:21:20 +0300

    making page temp layout added

--- src/modules/MakePage/Making.js
+++ src/modules/MakePage/Making.js
@@ -1,6 +1,21 @@
+import './styles/Making.scss';
+
 const Making = () => {
-    return (<div>
-        <img className="background-image" src="" alt="stuff n' things"/>
+    return (<div className="page making-content">
+        <img className="background-image" src={require("./bg-images/52-transformed.png")} alt="bg-images"/>
+        <div className="section-center">
+            <h2 className="shadowed-text">IMBUING YOU WITH <br/> SKILLS <br/> AND <br/> KNOWLEDGE</h2>
+        </div>
+        <div className="section-left">
+            <div className="skewed-content">
+                <h3 className="shadowed-text">INSPIRING TO <br/> INSPIRE OTHERS</h3>
+            </div> 
+        </div>
+        <div className="section-right">
+            <div className="skewed-content">
+                <h3 className="shadowed-text">PREPARING FOR <br/> THE FUTURE</h3>
+            </div> 
+        </div>
     </div>);
 }
 

e1e5740b37366c0d2cea3bceba118a2c971f2fed
matteo <matteo.yuen@gmail.com>
2024-12-01 16:00:20 +0300

    impact & everyone images set + animations

--- src/modules/EveryonePage/Everyone.js
+++ src/modules/EveryonePage/Everyone.js
@@ -3,9 +3,9 @@ import "./styles/Everyone.scss"
 const Everyone = () => {
     return <div className="page">
         <div className="skewed-bento everyone-bento">
-            <div className="grid-el grid-area-1 slide-from-top delay-100"></div>
-            <div className="grid-el grid-area-2 slide-from-top delay-200"></div>
-            <div className="grid-el grid-area-3 slide-from-top delay-300"></div>
+            <div className="everyone-bg1 grid-el grid-area-1 slide-from-top delay-200"></div>
+            <div className="everyone-bg2 grid-el grid-area-2 slide-from-top delay-400"></div>
+            <div className="everyone-bg3 grid-el grid-area-3 slide-from-top delay-600"></div>
         </div> 
     </div>
 }
--- src/modules/ImpactPage/ImpactPage.js
+++ src/modules/ImpactPage/ImpactPage.js
@@ -3,9 +3,9 @@ import "./styles/Impact.scss"
 const Impact = () => {
     return <div className="page">
         <div className="skewed-bento">
-            <div className="grid-el grid-area-1"></div>
-            <div className="grid-el grid-area-2"></div>
-            <div className="grid-el grid-area-3"></div>
+            <div className="impact-bg1 grid-el grid-area-1 slide-from-left delay-400"></div>
+            <div className="impact-bg2 grid-el grid-area-2 slide-from-left delay-500"></div>
+            <div className="impact-bg3 grid-el grid-area-3 slide-from-bottom delay-400"></div>
         </div> 
     </div>
 }
